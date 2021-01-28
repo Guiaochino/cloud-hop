122f4cc6cf7a4a08c882be98d59d75df1a1fe2b0
Guiaochino <chinotiamzon@gmail.com>
2021-01-28 09:09:15 +0800

    Constants.lua Fixed from repo

--- src/constants.lua
+++ src/constants.lua
@@ -10,3 +10,44 @@ BACKGROUND_SCROLL_SPEED = 20
 
 -- change when the bg changed
 BACKGROUND_LOOPING_POINT = 497
+
+-- global standard tile size
+TILE_SIZE = 16
+
+-- width and height of screen in tiles
+SCREEN_TILE_WIDTH = VIRTUAL_WIDTH / TILE_SIZE
+SCREEN_TILE_HEIGHT = VIRTUAL_HEIGHT / TILE_SIZE
+
+-- number of tiles in each tile set
+TILE_SET_WIDTH = 5
+TILE_SET_HEIGHT = 4
+
+-- number of tile sets in sheet
+TILE_SETS_WIDE = 6
+TILE_SETS_TALL = 10
+
+-- number of topper sets in sheet
+TOPPER_SETS_WIDE = 6
+TOPPER_SETS_TALL = 18
+
+-- total number of topper and tile sets
+TOPPER_SETS = TOPPER_SETS_WIDE * TOPPER_SETS_TALL
+TILE_SETS = TILE_SETS_WIDE * TILE_SETS_TALL
+
+-- player walking speed
+PLAYER_WALK_SPEED = 60
+
+-- player jumping velocity
+PLAYER_JUMP_VELOCITY = -230
+
+TILE_ID_EMPTY = 5
+TILE_ID_GROUND = 3
+
+player = { 
+    1, 2, 3, 4, 5
+}
+
+-- table of tiles that should trigger a collision
+COLLIDABLE_TILES = {
+    TILE_ID_GROUND
+}
