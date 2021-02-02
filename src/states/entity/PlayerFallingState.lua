e9fbcc24f159e9f1449c49f2bba2f6a553f111fe
Everly Torres <72683137+evertowers@users.noreply.github.com>
2021-02-02 20:57:33 +0800

    Update PlayerFallingState.lua

--- src/states/entity/PlayerFallingState.lua
+++ src/states/entity/PlayerFallingState.lua
@@ -6,8 +6,8 @@ function PlayerFallingState:init(player, gravity)
     self.player = player
     self.gravity = gravity
     self.animation = Animation {
-        frames = {5},
-        interval = 1
+        frames = {1},
+        interval = 0.1
     }
     self.player.currentAnimation = self.animation
 end
@@ -16,22 +16,8 @@ function PlayerFallingState:update(dt)
     self.player.currentAnimation:update(dt)
     self.player.dy = self.player.dy + self.gravity
     self.player.y = self.player.y + (self.player.dy * dt)
-
-  
-    -- if we get a collision beneath us, go into either walking or idle
-    if (tileBottomLeft and tileBottomRight) and (tileBottomLeft:collidable() or tileBottomRight:collidable()) then
-        self.player.dy = 0
         
-        -- set the player to be walking or idle on landing depending on input
-        if love.keyboard.isDown('left') or love.keyboard.isDown('right') then
-            self.player:changeState('walking')
-        else
-            self.player:changeState('idle')
-        end
-
-        self.player.y = (tileBottomLeft.y - 1) * TILE_SIZE - self.player.height
-     -- check side collisions and reset position
-    elseif love.keyboard.isDown('left') then
+    if love.keyboard.isDown('left') then
         self.player.direction = 'left'
         self.player.x = self.player.x - PLAYER_WALK_SPEED * dt
         self.player:checkLeftCollisions(dt)
@@ -39,7 +25,22 @@ function PlayerFallingState:update(dt)
         self.player.direction = 'right'
         self.player.x = self.player.x + PLAYER_WALK_SPEED * dt
         self.player:checkRightCollisions(dt)
-    elseif love.keyboard.wasPressed('space') then
-        self.player:changeState('jump')
     end
+
+    for k, object in pairs(self.player.level.objects) do
+        if object:collides(self.player) then
+            if object.solid then
+                self.player:changeState('idle')
+                if love.keyboard.isDown('left') or love.keyboard.isDown('right') then
+                    self.player:changeState('walking')
+                else
+                    self.player:changeState('idle')
+                end
+            elseif object.consumable then
+                object.onConsume(self.player)
+                table.remove(self.player.level.objects, k)
+            end
+        end
+    end
+
 end
