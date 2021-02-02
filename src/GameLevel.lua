c728ec8b8cb13a3fb5132b92239bb32bea539101
Everly Torres <72683137+evertowers@users.noreply.github.com>
2021-02-02 21:07:18 +0800

    Add files via upload

---
+++ src/GameLevel.lua
@@ -0,0 +1,54 @@
+--[[
+    GD50
+    Super Mario Bros. Remake
+
+    -- GameLevel Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GameLevel = Class{}
+
+
+function GameLevel:init(entities, objects)
+    self.entities = entities
+    self.objects = objects
+end
+
+--[[
+    Remove all nil references from tables in case they've set themselves to nil.
+]]
+function GameLevel:clear()
+    for i = #self.objects, 1, -1 do
+        if not self.objects[i] then
+            table.remove(self.objects, i)
+        end
+    end
+
+    for i = #self.entities, 1, -1 do
+        if not self.objects[i] then
+            table.remove(self.objects, i)
+        end
+    end
+end
+
+function GameLevel:update(dt)
+    for k, object in pairs(self.objects) do
+        object:update(dt)
+    end
+
+    for k, entity in pairs(self.entities) do
+        entity:update(dt)
+    end
+end
+
+function GameLevel:render()
+    for k, object in pairs(self.objects) do
+        object:render()
+    end
+
+    for k, entity in pairs(self.entities) do
+        entity:render()
+    end
+end
