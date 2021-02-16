8e7a78a26386cbca14d1e719b238916c485c9a61
Guiaochino <chinotiamzon@gmail.com>
2021-02-16 18:48:25 +0800

    Settings Screen UI Fixed and Finished

--- src/states/SettingScreenState.lua
+++ src/states/SettingScreenState.lua
@@ -1,7 +1,9 @@
 SettingScreenState = Class{__includes = BaseState}
 
 function SettingScreenState:update(dt)
-    
+    if love.keyboard.wasPressed('escape') then
+        gStateMachine:change('title')
+    end
 end
 
 function SettingScreenState:render()
@@ -10,11 +12,28 @@ function SettingScreenState:render()
 
     -- Design for UI goes here
     love.graphics.setColor(0, 0, 0, 0.5)
-    love.graphics.rectangle('fill', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46)
+    love.graphics.rectangle('fill', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46, 30, 30)
     love.graphics.setColor(0, 0, 0, 1)
-    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46)
-    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46)
-    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46)
+    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46, 30, 30)
+    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46, 30, 30)
+    love.graphics.rectangle('line', 10, 36 , VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT - 46, 30, 30)
 
     -- Display Audio and SFX for Silent
+    -- Checkboxes
+    love.graphics.rectangle('line', 50, 36 * 2, 10, 10)
+    love.graphics.rectangle('line', 50, 36 + (36 * 2), 10, 10)
+
+    -- Display text
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.printf("Audio", 2, VIRTUAL_HEIGHT / 2 - 90, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf("SFX", 2, VIRTUAL_HEIGHT / 2 - 55, VIRTUAL_WIDTH, 'center')
+
+    --Display Highscore
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.printf("HIGHSCORE : 0", 1, (VIRTUAL_HEIGHT / 2) - 10, VIRTUAL_WIDTH, 'center')
+
+    -- Display button
+    love.graphics.rectangle('line', 20, VIRTUAL_HEIGHT - 76, VIRTUAL_WIDTH - 40, 46, 15, 15)
+    love.graphics.rectangle('line', 20, VIRTUAL_HEIGHT - 76, VIRTUAL_WIDTH - 40, 46, 15, 15)
+    love.graphics.rectangle('line', 20, VIRTUAL_HEIGHT - 76, VIRTUAL_WIDTH - 40, 46, 15, 15)
 end
